0287827f4492570b895d56c9218b726cda54dd72
Walter Nhaca <nhacawalter@gmail.com>
2025-02-08 16:32:42 +0000

    modificado a dependecia loombok, criado o metodo post e adcionado atributo id ao dto pra facilitar o post

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -4,10 +4,13 @@ package com.example.crud.controllers;
 import com.example.crud.domain.Product;
 import com.example.crud.domain.ProductRepository;
 import com.example.crud.domain.RequestProduct;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController // marca a classe como um controller
 @RequestMapping("/product") //especifica a url que o metodo controlador ira manipular
@@ -30,5 +33,20 @@ public class ProductController {
         Product newProduct = new Product(data); // pega o dto e cria o objecto
         repository.save(newProduct); //persiste na database
         return ResponseEntity.ok().build(); //quando nao se coloca nada dentro do statsus ok() temos que colocar build() no final
+    }
+
+    @PutMapping
+    @Transactional //quando faremos mais de uma operacao e esperamos por um resultado
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else{
+            return  ResponseEntity.notFound().build();
+        }
+
     }
 }
--- src/main/java/com/example/crud/domain/RequestProduct.java
+++ src/main/java/com/example/crud/domain/RequestProduct.java
@@ -1,5 +1,13 @@
 package com.example.crud.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 //o record eh uma forma de criar uma classe que so tem artibutos sem implementacao de nada tambem eh chamdao dto(data transfer object- pra receber ou tranferir dados para o cliente)
-public record RequestProduct(String name, Integer price_in_cents) {
+public record RequestProduct(
+        String id,
+        @NotBlank //validacao pra que nao seja vazio
+        String name,
+        @NotNull //validacao pra que nao seja nulo
+        Integer price_in_cents) {
 }
